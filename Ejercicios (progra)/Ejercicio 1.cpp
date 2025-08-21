c2624055d1294fdd5df1fbbfae7834b29a04d62e
Samuel3182 <samuel.zarate.z@ucb.edu.bo>
2025-08-21 19:05:58 -0400

    ejercicios de curso (Samuel Zarate)

---
+++ Ejercicios (progra)/Ejercicio 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int valor1 = 6;
+    int valor2 = 14;
+    if(valor1 > valor2)
+    {
+        cout << valor1 << " es mayor a: "<< valor2;
+    }
+    else
+    {
+        cout << valor1 << " es menor igual a: "<< valor1; 
+    }
+
+    return 0;
+
+}
---
+++ Ejercicios (progra)/Practica 1.cpp
@@ -0,0 +1,12 @@
+//Practica con Visual studio code
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    cout <<"Hola cara de bola";
+    return 0;
+
+}
